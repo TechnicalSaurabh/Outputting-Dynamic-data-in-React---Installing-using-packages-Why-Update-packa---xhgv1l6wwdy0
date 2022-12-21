712935a3645f3ec1c0629d429f80d0ccf08f24a4
Saurabh Yadav <69469177+TechnicalSaurabh@users.noreply.github.com>
2022-12-21 13:05:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,22 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+const [name,setName] = useState('____');
+let text='';
+const onChange=(e)=>{
+ text = e.target.value;
+}
+const onClick = () =>{
+  if(text.length>0)
+setName(text);
+else
+ setName('____');
+}
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={onChange}></input>
+      <button id='button' onClick={onClick}>Click</button>
+      <p id='text'> Hello my name is {name} and I study at Newton School</p>
     </div>
   )
 }
